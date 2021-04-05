3c8d797265eed108ca6e670dc9f2f4a74c0b2c90
Brianna Birman <brianna.birman@salesforce.com>
2021-04-05 00:11:02 -0700

    Build for catalyst

--- build_for_sdk.sh
+++ build_for_sdk.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 xcodebuild BITCODE_GENERATION_MODE=bitcode OTHER_CFLAGS="\$(inherited) -fembed-bitcode" ARCHS="arm64 arm64e" ONLY_ACTIVE_ARCH=NO PLATFORM_NAME=iphoneos -configuration Release -project sqlcipher.xcodeproj -target sqlcipher clean install
 if [ -f "build/UninstalledProducts/iphoneos/libsqlcipher.a" ]; then
-	mv build/UninstalledProducts/iphoneos/libsqlcipher.a libsqlcipher-iphoneos.a
+       mv build/UninstalledProducts/iphoneos/libsqlcipher.a libsqlcipher-iphoneos.a
 fi
 xcodebuild BITCODE_GENERATION_MODE=bitcode OTHER_CFLAGS="\$(inherited) -fembed-bitcode" ARCHS="x86_64" ONLY_ACTIVE_ARCH=NO PLATFORM_NAME=iphonesimulator -configuration Release -sdk iphonesimulator -project sqlcipher.xcodeproj -target sqlcipher clean install
 if [ -f "./build/UninstalledProducts/iphonesimulator/libsqlcipher.a" ]; then
-	mv ./build/UninstalledProducts/iphonesimulator/libsqlcipher.a libsqlcipher-iphonesimulator.a
+       mv ./build/UninstalledProducts/iphonesimulator/libsqlcipher.a libsqlcipher-iphonesimulator.a
 fi
-lipo -create libsqlcipher-iphoneos.a libsqlcipher-iphonesimulator.a -output libsqlcipher.a
+
+xcodebuild BITCODE_GENERATION_MODE=bitcode OTHER_CFLAGS="\$(inherited) -fembed-bitcode" ONLY_ACTIVE_ARCH=NO -destination "generic/platform=macOS,variant=Mac Catalyst,name=Any Mac" -derivedDataPath "./build-catalyst" -configuration Release -project sqlcipher.xcodeproj -scheme sqlcipher clean build
+if [ -f "./build-catalyst/Build/Products/Release-maccatalyst/libsqlcipher.a" ]; then
+    mv build-catalyst/Build/Products/Release-maccatalyst/libsqlcipher.a libsqlcipher-catalyst.a
+fi
+
+xcodebuild -create-xcframework -output SQLCipher.xcframework \
+    -library libsqlcipher-iphoneos.a \
+    -library libsqlcipher-iphonesimulator.a \
+    -library libsqlcipher-catalyst.a
